8f281247f528eae33830013e994881a99f69c6bc
Nathan <nathanbogale@gmail.com>
2023-04-24 20:21:25 +0000

    mods

--- auth/src/__tests__/index.test.ts
+++ auth/src/__tests__/index.test.ts
@@ -2,7 +2,5 @@ import request from 'supertest';
 import app from '../app';
 
 it('Reponds with a satatus of 200', () => {
-  request(app)
-  .get('/')
-  .expect(200)
+  request(app).get('/').expect(200);
 });
